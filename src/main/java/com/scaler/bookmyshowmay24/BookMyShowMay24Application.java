2bf0e453348a05e2b8ad28476a0426fdd27ba6f2
Shivam Kendre <61688297+shivamkendre@users.noreply.github.com>
2024-05-08 11:31:44 +0530

    Book Show Feature Added

---
+++ src/main/java/com/scaler/bookmyshowmay24/BookMyShowMay24Application.java
@@ -0,0 +1,13 @@
+package com.scaler.bookmyshowmay24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowMay24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowMay24Application.class, args);
+    }
+
+}
